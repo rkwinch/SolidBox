87a763103ce00168644ec5261a5e2119b4b55c35
Robin <rkwinch@g.clemson.edu>
2019-07-28 22:15:53 -0500

    add missing file

--- SolidBox.h
+++ SolidBox.h
@@ -2,24 +2,26 @@
 
 #include "SquarePlane.h"
 #include "Shape.h"
-#include "Channel.h"
 #include <string>
 #include <set>
+#include "ConnectionChannel.h"
 
 class SolidBox : public Shape {
 	
 public:
-	SolidBox(int sideLength);
+	SolidBox(double sideLength);
 	std::string GetShapeName() override;
 	static void PrintCubeSet();
-
+	double SolidBox::GetSideLength();
+	ConnectionChannel GetConnChannel();
 
 private:
-	
+	double sideLength;
+	ConnectionChannel channel;
+	bool hasConnection; // flag for checking if the SolidBox has a connection
 	std::string name;
 	static int nameIDCounter; // used for naming unique cubes
 	static std::set<std::string> cubeNames; // helps enforce uniqueness of cubes created
 	static const int planesPerSolidBox;
-	std::vector<SquarePlane> groupOfPlanes;
 };
 
